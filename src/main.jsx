1936f77ee7bf3ed69683e17b3fe595d77d53e0de
anshuthopsee <thopseeanshu@gmail.com>
2023-10-05 03:01:11 +0100

    testing

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+      <p>test</p>
       <BrowserRouter 
         basename={import.meta.env.DEV ? '/' : '/weather-now/'}
       >
