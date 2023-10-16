32a3ad9f89fd79a94839366437755fd23270e7f7
anshuthopsee <thopseeanshu@gmail.com>
2023-10-16 19:51:58 +0100

    minor fix

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,12 +7,11 @@ function Layout() {
     <>
       <div className="flex min-h-screen bg-gradient-to-t from-custom-gradient-start to-custom-gradient-end flex-col">
         <Header/>
-        <div className='container mx-auto px-4 pb-4 grow flex flex-col'>
+        <div className='container mx-auto px-4 grow flex flex-col'>
           <div className='flex-1'>
             <Outlet/>
           </div>
           <div className='flex w-full p-4 h-[60px] flex-0 text-white justify-center items-center'>
-            
             <a href="https://github.com/anshuthopsee/weather-now" className='flex gap-2 h-fit item font-bold underline cursor-pointer '>
               <FaGithub className='text-[28px] text-white cursor-pointer'/>
               <p className='text-[14px] mt-[3px]'>anshuthopsee/weather-now</p>
